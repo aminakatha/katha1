81b001dc3673e2207a233748edc6dda95ebe39e1
Amina Islam Katha <140314069+aminakatha@users.noreply.github.com>
2023-07-30 14:46:49 +0600

    Add files via upload

---
+++ katha1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main ()
+
+{
+
+    cout<<"Hello in my university!!!"<<endl<<endl;
+    cout<<"Im a student in it faculty"<<endl<<endl;
+    cout<<"Welcome";
+}
